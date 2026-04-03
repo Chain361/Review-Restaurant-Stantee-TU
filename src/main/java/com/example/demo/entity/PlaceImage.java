994ec661823c31da4a57efa2ba4dac5df446a85c
Rickoroxd <0954744487zaz@gmail.com>
2026-04-03 11:03:03 +0700

    Fix Sercurity for review/create

--- src/main/java/com/example/demo/entity/PlaceImage.java
+++ src/main/java/com/example/demo/entity/PlaceImage.java
@@ -1,5 +1,23 @@
 package com.example.demo.entity;
-
+import jakarta.persistence.*;
+import lombok.Data;
+@Entity
+@Table(name = "place_images")
+@Data
 public class PlaceImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "placeid")
+    private Place place;
+
+    private String fileName;
+    private String filePath;
+
+    public PlaceImage() {}
     
+    public Place getPlace() { return place; }
+    public void setPlace(Place place) { this.place = place; }
 }
