b52185af879ec2822b86d2897bd89140c14469bf
Chain361 <jsonbrody5@gmail.com>
2026-04-03 15:03:41 +0700

    rating using float type

--- src/main/java/com/example/demo/entity/Review.java
+++ src/main/java/com/example/demo/entity/Review.java
@@ -16,7 +16,7 @@ public class Review {
     private LocalDate reviewDate;
 
     @Column(name = "\"rating\"")
-    private Integer rating;
+    private Float rating;
 
     @Column(name = "\"comment\"")
     private String comment;
@@ -47,11 +47,11 @@ public class Review {
         this.reviewDate = reviewDate;
     }
 
-    public Integer getRating() {
+    public Float getRating() {
         return rating;
     }
 
-    public void setRating(Integer rating) {
+    public void setRating(Float rating) {
         this.rating = rating;
     }
 
